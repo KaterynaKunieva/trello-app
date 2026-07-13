6e33a51dd8b3030bd4d2db0d0dc302eae56b3597
Kateryna <kunieva.kateryna@gmail.com>
2026-07-13 23:56:18 +0300

    build: switched router to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Board from './pages/Board/Board';
 import Default from './pages/Default/Default';
 
 function App(): React.JSX.Element {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/board" element={<Board />} />
         <Route path="/" element={<Default />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
